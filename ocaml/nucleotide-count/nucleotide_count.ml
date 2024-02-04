903af788cbdb3a622378c4d8b7f9664221487324
Juan Gutierrez <juanny.gee@gmail.com>
2024-02-04 13:25:55 -0500

    Complete count_nucleotides implementation

--- ocaml/nucleotide-count/nucleotide_count.ml
+++ ocaml/nucleotide-count/nucleotide_count.ml
@@ -23,5 +23,15 @@ let count_nucleotide s c =
     in count (String.to_list s) 0
 
 let count_nucleotides s =
-  let t = Map.set ~key:'A' ~data:1 empty in
-  Result.Ok t
+  let rec count s_list m =
+    match s_list with
+    | [] -> Result.Ok m
+    | hd :: tl ->
+      if not (c_valid hd) then Result.Error hd
+      else if Map.mem m hd then count tl m
+      else
+        let x res = match res with
+          | Result.Ok num -> count tl (Map.set ~key:hd ~data:num m)
+          | Result.Error c -> Result.Error c (* If we match an error, return it - this happens when invalid char encountered in the middle of nucleotide  *)
+        in x (count_nucleotide s hd)
+  in count (String.to_list s) empty
