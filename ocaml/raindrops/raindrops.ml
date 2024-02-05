b1774832f9345aff9e2270fa9ea86ddbb4cdfa22
Juan Gutierrez <juanny.gee@gmail.com>
2024-02-04 19:11:48 -0500

    Solution for raindrops

--- ocaml/raindrops/raindrops.ml
+++ ocaml/raindrops/raindrops.ml
@@ -1,2 +1,12 @@
-let raindrop _ =
-    failwith "'raindrop' is missing"
+let pling n =
+  if n mod 3 = 0 then "Pling" else ""
+
+let plang n =
+  if n mod 5 = 0 then "Plang" else ""
+
+let plong n =
+  if n mod 7 = 0 then "Plong" else ""
+
+let raindrop n =
+  let s = String.concat "" [pling n; plang n; plong n] in
+  if s = "" then string_of_int n else s
