b07fe65a0344044dab13ab21331bc27f78063ab0
Juan Gutierrez <juanny.gee@gmail.com>
2024-02-05 15:41:38 -0500

    Implement rna transcription

--- ocaml/rna-transcription/rna_transcription.ml
+++ ocaml/rna-transcription/rna_transcription.ml
@@ -1,5 +1,12 @@
 type dna = [ `A | `C | `G | `T ]
 type rna = [ `A | `C | `G | `U ]
 
-let to_rna _ =
-    failwith "'to_rna' is missing"
+let to_rna dnaList =
+  let reMap d =
+  match d with
+  | `G -> `C
+  | `C -> `G
+  | `T -> `A
+  | `A -> `U
+  in
+  List.map reMap dnaList
