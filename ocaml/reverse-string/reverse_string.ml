bbc39dfc88e521dfa18b8269feccd7ef15c899a1
Juan Gutierrez <juanny.gee@gmail.com>
2024-02-04 19:44:17 -0500

    Implement reverse-string

--- ocaml/reverse-string/reverse_string.ml
+++ ocaml/reverse-string/reverse_string.ml
@@ -1,2 +1,5 @@
-let reverse_string _ =
-    failwith "'reverse_string' is missing"
+let reverse_string s =
+  let rec rev new_s i =
+    if i < 0 then new_s
+    else rev (new_s ^ String.make 1 s.[i]) (i - 1)
+  in rev "" (String.length s - 1)
