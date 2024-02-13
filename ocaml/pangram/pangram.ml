f6c2c945876a30ef2e0b4cdaedeaccca1265832e
Juan Gutierrez <juanny.gee@gmail.com>
2024-02-13 15:09:08 -0500

    Minor tweak

--- ocaml/pangram/pangram.ml
+++ ocaml/pangram/pangram.ml
@@ -1,13 +1,17 @@
+let is_char c =
+  match c with
+  | 'a'..'z' -> true
+  | _ -> false
+
 let is_pangram s =
   let lowered = String.lowercase_ascii s in
   let c_list = List.init (String.length lowered) (String.get lowered) in
+  let filtered_c_list = List.filter is_char c_list in
   let rec f c' acc =
     match c' with
     | [] -> acc
     | c :: t ->
-      match c with
-      | 'a'..'z' -> match List.find_opt (fun v -> v = c) acc with
-        | Some _ -> f t acc
-        | None -> f t (c :: acc)
-      | _ -> f t acc
-  in List.length (f c_list []) = 26
+      match List.find_opt (fun v -> v = c) acc with
+      | Some _ -> f t acc
+      | None -> f t (c :: acc)
+  in List.length (f filtered_c_list []) = 26
