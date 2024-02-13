238616458f44850d7a462d8cf164dca4b6385a28
Juan Gutierrez <juanny.gee@gmail.com>
2024-02-13 15:01:47 -0500

    Implement pangram

--- ocaml/pangram/pangram.ml
+++ ocaml/pangram/pangram.ml
@@ -1,2 +1,13 @@
-let is_pangram _ =
-    failwith "'is_pangram' is missing"
+let is_pangram s =
+  let lowered = String.lowercase_ascii s in
+  let c_list = List.init (String.length lowered) (String.get lowered) in
+  let rec f c' acc =
+    match c' with
+    | [] -> acc
+    | c :: t ->
+      match c with
+      | 'a'..'z' -> match List.find_opt (fun v -> v = c) acc with
+        | Some _ -> f t acc
+        | None -> f t (c :: acc)
+      | _ -> f t acc
+  in List.length (f c_list []) = 26
